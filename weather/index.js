563b02907fe5d98b8e612820eef68329b87a597d
ao0iwh4awp4 <47169269+whatland@users.noreply.github.com>
2022-03-08 17:32:14 -0800

    done

--- weather/index.js
+++ weather/index.js
@@ -4,6 +4,9 @@ const botconfig = require("./botconfig.json");
 const fetch = require("node-fetch");
 
 
+var lat;
+var lon;
+
 bot.on("ready", async () => {
     console.log(`online`)
 })
@@ -17,26 +20,50 @@ bot.on("message", async msg=>  {
     let msgArray = msg.content.split(" ");
     let cmd = msgArray[0];
 
-    if(cmd == `${prefix}weather`)
+    if(cmd == `${prefix}lat`)
     {
-        
-        // use for input to the api
-
-        var newStr = "";
-
+    
         var arg2 = msg.content.slice(prefix.length).split(' ');
 
         var argRep = arg2[1]
 
+        lat = argRep;
+
+        console.log(lat);
+
+
+        msg.channel.send("set");
+    }
+
+
+    if(cmd == `${prefix}long`)
+    {
+    
+        var arg2 = msg.content.slice(prefix.length).split(' ');
+
+        var argRep = arg2[1]
+
+        lon = argRep;
+
+        console.log(lon);
+
+        msg.channel.send("set");
+    }
+
+
+
+    if(cmd == `${prefix}weather`)
+    {
+        
         //
 
-        var website = "https://fcc-weather-api.glitch.me/api/current?lat=56.613701&lon=-134.190933";
+        var website = "https://fcc-weather-api.glitch.me/api/current?lat=" + lat + "&lon=" + lon;
 
 
         const response = await fetch(website);
         const data = await response.json();
 
-        console.log(data.coord["lon"]);
+        
 
         msg.channel.send("Coordinates of Weather are: " + data.coord["lon"] + " longitude and " + data.coord["lat"] + " latitude");
         msg.channel.send("Temperature (CELCIUS): " + data.main["temp"]);
@@ -44,6 +71,8 @@ bot.on("message", async msg=>  {
         msg.channel.send("Location Name (Verify): " + data.name);
 
     }
+
+    
 
 })
 
